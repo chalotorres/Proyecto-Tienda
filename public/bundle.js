cda6d103b7290e9306bcd3b73c3437fd85dc868d
Hazael Torres <hazaeltorres09@gmail.com>
2024-07-10 15:16:48 -0600

    Cargando los productos en el carrito

--- public/bundle.js
+++ public/bundle.js
@@ -66,13 +66,71 @@ const botonesCerrarCarrito = document.querySelectorAll('[data-accion="cerrar-car
 const btnAgregarCarrito = document.getElementById('agregar-al-carrito');
 // Se obtiene el producto actual
 const producto = document.getElementById("producto");
+// Arreglo que contiene los productos del carrito
+const carrito = [];
 
 // variable que obtiene la ventana del carrito
 const ventanaCarrito = document.getElementById('carrito');
 
 // Función que mostrará la ventana del carrito
-const renderCarrito = () => {
+const renderCarrito = () => {   
+    // Se agrega la clase al elemento de la venta que permite que sea visible
     ventanaCarrito.classList.add('carrito--active');
+    
+    // Parte para limpiar la lsita de elementos que se muestran en el carrito
+    const productosAnteriores = ventanaCarrito.querySelectorAll('.carrito__producto');
+    productosAnteriores.forEach(producto => producto.remove());
+
+    // Iteramos sobre cada producto para agregarlo al carrito y mostrarlo
+    carrito.forEach((productoCarrito) => {
+        // Se obtiene el url del thumb del producto seleccionado
+        let thumbSrc = producto.querySelectorAll('.producto__thumb-img')[0].src;
+        // Se verifica si hubo un cambio de color en el producto
+        if(productoCarrito.color === 'rojo') {
+            thumbSrc = './img/thumbs/rojo.jpg';
+        } else if(productoCarrito.color === 'amarillo') {
+            thumbSrc = './img/thumbs/amarillo.jpg';
+        }
+        // Plantilla del código HTML para el producto
+        const plantillaProducto = `
+            <div class="carrito__producto-info">
+                <img src="${thumbSrc}" alt="" class="carrito__thumb" />
+                <div>
+                    <p class="carrito__producto-nombre">
+                        <span class="carrito__producto-cantidad">${productoCarrito.cantidad} x </span>${productoCarrito.nombre}
+                    </p>
+                    <p class="carrito__producto-propiedades">
+                        Tamaño:<span>${productoCarrito.tamaño}</span> Color:<span>${productoCarrito.color}</span>
+                    </p>
+                </div>
+            </div>
+            <div class="carrito__producto-contenedor-precio">
+                <button class="carrito__btn-eliminar-item" data-accion="eliminar-item-carrito">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
+                        />
+                    </svg>
+                </button>
+                <p class="carrito__producto-precio">$500.00</p>
+            </div>
+        `;
+        
+        // Se creará el div que contendrá los productos del carrito
+        const itemCarrito = document.createElement('div');
+        // Se agrega la clase al div del producto del carrito
+        itemCarrito.classList.add('carrito__producto');
+        // se agrega el contenido de la plantilla al html
+        itemCarrito.innerHTML = plantillaProducto;
+        // Se agrega en el lugar correspondiente el div creado
+        ventanaCarrito.querySelector('.carrito__body').appendChild(itemCarrito);
+    });
 };
 
 // Funcionalidad para abrir carrito
@@ -96,9 +154,18 @@ botonesCerrarCarrito.forEach((boton) => {
 // Funcionalidad para agregar productos al carrito
 btnAgregarCarrito.addEventListener('click', (e) => {
     // -- Valores del producto
-    producto.dataset.productoId;
-    producto.querySelector('.producto__nombre').innerText;
-    parseInt(producto.querySelector('#cantidad').value);
-    producto.querySelector('#propiedad-color input:checked').value;
-    producto.querySelector('#propiedad-tamaño input:checked').value;
+    const id = producto.dataset.productoId;
+    const nombre = producto.querySelector('.producto__nombre').innerText;
+    const cantidad = parseInt(producto.querySelector('#cantidad').value);
+    const color = producto.querySelector('#propiedad-color input:checked').value;
+    const tamaño = producto.querySelector('#propiedad-tamaño input:checked').value;
+
+    // Se agrega el producto con sus valores al carrito
+    carrito.push({
+        id: id,
+        nombre: nombre,
+        cantidad: cantidad,
+        color: color,
+        tamaño: tamaño,
+    });
 });
