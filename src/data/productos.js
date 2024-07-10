2d99b2c379325364595f927701081174d2b3cc9d
Hazael Torres <hazaeltorres09@gmail.com>
2024-07-10 14:12:58 -0600

    Simulando una base de datos

---
+++ src/data/productos.js
@@ -0,0 +1,20 @@
+export default {
+    productos: [
+        {
+            id: '1',
+            nombre: 'Tennis Converse Standard.',
+            descripcion: 'Lorem ipsum dolor sit amet.',
+            precio: 500.0,
+            colores: ['negro', 'rojo', 'amarillo'],
+            tamaños: ['1,5', '2', '2,5', '3', '4'],
+        },
+        {
+            id: '2',
+            nombre: 'Tennis Converse Chuck Taylor All Star Lift Platform Leather.',
+            descripcion: 'Lorem ipsum dolor sit amet.',
+            precio: 650.0,
+            colores: ['negro', 'rojo', 'amarillo'],
+            tamaños: ['1,5', '2', '2,5', '3', '4'],
+        },
+    ]
+}
