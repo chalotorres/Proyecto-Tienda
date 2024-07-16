59cfcf5f125118bd40d69bd73843cdb61bbbf319
Hazael Torres <hazaeltorres09@gmail.com>
2024-07-16 11:02:09 -0600

    Interfaz del carrito vacio

--- public/bundle.js
+++ public/bundle.js
@@ -106,64 +106,78 @@ const renderCarrito = () => {
     const productosAnteriores = ventanaCarrito.querySelectorAll('.carrito__producto');
     productosAnteriores.forEach(producto => producto.remove());
 
-    // Iteramos sobre cada producto para agregarlo al carrito y mostrarlo
-    carrito.forEach((productoCarrito) => {
-        // Se busca el producto con su ID de la BD
-        data.productos.forEach((productoBD) => {
-            if(productoBD.id === productoCarrito.id) {
-                // Se actualiza el productoCarrito con el precio
-                productoCarrito.precio = productoBD.precio;
+    let total = 0;
+
+    // Se comprueba si hay productos
+    if(carrito.length < 1) {
+        // Si no hay elementos se agrega la clase que muestra el aviso
+        ventanaCarrito.classList.add('carrito--vacio');
+    } else {
+        // Si el carrito tiene elementos se quita el aviso
+        ventanaCarrito.classList.remove('carrito--vacio');
+
+        carrito.forEach((productoCarrito) => {
+            // Se busca el producto con su ID de la BD (prducto.js)
+            data.productos.forEach((productoBD) => {
+                if(productoBD.id === productoCarrito.id) {
+                    // Se actualiza el productoCarrito con el precio
+                    productoCarrito.precio = productoBD.precio;
+
+                    total += productoBD.precio * productoCarrito.cantidad;
+                }
+            });
+            
+            // Se obtiene el url del thumb del producto seleccionado
+            let thumbSrc = producto.querySelectorAll('.producto__thumb-img')[0].src;
+            // Se verifica si hubo un cambio de color en el producto
+            if(productoCarrito.color === 'rojo') {
+                thumbSrc = './img/thumbs/rojo.jpg';
+            } else if(productoCarrito.color === 'amarillo') {
+                thumbSrc = './img/thumbs/amarillo.jpg';
             }
-        });
-        
-        // Se obtiene el url del thumb del producto seleccionado
-        let thumbSrc = producto.querySelectorAll('.producto__thumb-img')[0].src;
-        // Se verifica si hubo un cambio de color en el producto
-        if(productoCarrito.color === 'rojo') {
-            thumbSrc = './img/thumbs/rojo.jpg';
-        } else if(productoCarrito.color === 'amarillo') {
-            thumbSrc = './img/thumbs/amarillo.jpg';
-        }
-        // Plantilla del código HTML para el producto
-        const plantillaProducto = `
-            <div class="carrito__producto-info">
-                <img src="${thumbSrc}" alt="" class="carrito__thumb" />
-                <div>
-                    <p class="carrito__producto-nombre">
-                        <span class="carrito__producto-cantidad">${productoCarrito.cantidad} x </span>${productoCarrito.nombre}
-                    </p>
-                    <p class="carrito__producto-propiedades">
-                        Tamaño:<span>${productoCarrito.tamaño}</span> Color:<span>${productoCarrito.color}</span>
-                    </p>
+            // Plantilla del código HTML para el producto
+            const plantillaProducto = `
+                <div class="carrito__producto-info">
+                    <img src="${thumbSrc}" alt="" class="carrito__thumb" />
+                    <div>
+                        <p class="carrito__producto-nombre">
+                            <span class="carrito__producto-cantidad">${productoCarrito.cantidad} x </span>${productoCarrito.nombre}
+                        </p>
+                        <p class="carrito__producto-propiedades">
+                            Tamaño:<span>${productoCarrito.tamaño}</span> Color:<span>${productoCarrito.color}</span>
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="carrito__producto-contenedor-precio">
-                <button class="carrito__btn-eliminar-item" data-accion="eliminar-item-carrito">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                            d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
-                        />
-                    </svg>
-                </button>
-                <p class="carrito__producto-precio">${formatearMoneda.format( productoCarrito.precio * productoCarrito.cantidad)}</p>
-            </div>
-        `;
-        
-        // Se creará el div que contendrá los productos del carrito
-        const itemCarrito = document.createElement('div');
-        // Se agrega la clase al div del producto del carrito
-        itemCarrito.classList.add('carrito__producto');
-        // se agrega el contenido de la plantilla al html
-        itemCarrito.innerHTML = plantillaProducto;
-        // Se agrega en el lugar correspondiente el div creado
-        ventanaCarrito.querySelector('.carrito__body').appendChild(itemCarrito);
-    });
+                <div class="carrito__producto-contenedor-precio">
+                    <button class="carrito__btn-eliminar-item" data-accion="eliminar-item-carrito">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
+                            />
+                        </svg>
+                    </button>
+                    <p class="carrito__producto-precio">${formatearMoneda.format( productoCarrito.precio * productoCarrito.cantidad)}</p>
+                </div>
+            `;
+            
+            // Se creará el div que contendrá los productos del carrito
+            const itemCarrito = document.createElement('div');
+            // Se agrega la clase al div del producto del carrito
+            itemCarrito.classList.add('carrito__producto');
+            // se agrega el contenido de la plantilla al html
+            itemCarrito.innerHTML = plantillaProducto;
+            // Se agrega en el lugar correspondiente el div creado
+            ventanaCarrito.querySelector('.carrito__body').appendChild(itemCarrito);
+        });
+    }
+
+    ventanaCarrito.querySelector('.carrito__total').innerText = formatearMoneda.format(total);
 };
 
 // Funcionalidad para abrir carrito
@@ -177,12 +191,11 @@ botonesAbrirCarrito.forEach((boton) => {
 
 // Funcionalidad para cerrar carrito
 botonesCerrarCarrito.forEach((boton) => {
-    // Evento para los botones que cerrarán el carrito
-    boton.addEventListener('click', (e) =>{
+    // Evento para los botones que cerrarán el carrito  
+    boton.addEventListener('click', (e) => {
         // Llamada a la función que cierra el carrito
-        console.log('Entra');
-        ventanaCarrito.classList.remove('carrito--active');
-    });
+		ventanaCarrito.classList.remove('carrito--active');
+	});
 });
 
 // Funcionalidad para agregar productos al carrito
@@ -244,6 +257,6 @@ ventanaCarrito.addEventListener('click', (e) => {
                 return item;
             }
         });
+        renderCarrito();
     }
-    renderCarrito();
 });
